2d7e975394c9454893b7267ea74c43acdc5dc057
Daniel_Macias <damacias24@itp.edu.co>
2026-03-18 21:25:38 -0500

    Modulo categoria completo

--- src/modules/categorias/categorias.module.ts
+++ src/modules/categorias/categorias.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { CategoriasService } from './categorias.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriasController } from './categorias.controller';
-
+import { CategoriasService } from './categorias.service';
+import { Categoria } from './entities/categoria.entity';
+ 
 @Module({
+  // Registra la entidad para que TypeORM la gestione en este módulo
+  imports: [TypeOrmModule.forFeature([Categoria])],
   controllers: [CategoriasController],
   providers: [CategoriasService],
+  // Exportamos el servicio por si otros módulos necesitan
+  // consultar el precio base de una categoría (ej: módulo de Facturación)
+  exports: [CategoriasService],
 })
 export class CategoriasModule {}
+ 
