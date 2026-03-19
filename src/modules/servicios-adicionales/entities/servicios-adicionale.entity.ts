334d1a9f92ac4bb84e8de6ce24b23a212b32aa07
jamilton legarda <jamiltonlegarda14@gmail.com>
2026-03-18 19:58:23 -0500

    entity de servicios-adicionales

---
+++ src/modules/servicios-adicionales/entities/servicios-adicionale.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+    
+@Entity('servicios_adicionales')
+export class ServiciosAdicional {
+    @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  nombre?: string;
+
+  @Column('decimal')
+  precio?: number;
+
+  @Column({ default: true })
+  activo?: boolean;
+}
