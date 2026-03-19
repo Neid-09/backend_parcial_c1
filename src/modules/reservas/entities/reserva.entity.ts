90f73c4dc3e166ba2148854dec3c733ebb8996c7
aplicacionJava <jlenriquez24@itp.edu.co>
2026-03-18 20:01:31 -0500

    entidades

--- src/modules/reservas/entities/reserva.entity.ts
+++ src/modules/reservas/entities/reserva.entity.ts
@@ -1,1 +1,22 @@
-export class Reserva {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Reserva {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'date' })
+  fecha_inicio: Date;
+
+  @Column({ type: 'date' })
+  fecha_fin: Date;
+
+  @Column()
+  huespedId: number;
+  // 🔗 Relación: Reserva pertenece a un Huésped (N:1)
+
+  @Column()
+  habitacionId: number;
+  // 🔗 Relación: Reserva pertenece a una Habitación (N:1)
+}
